dc655e57b2c952c40d0313fa4dc69e5d486cd085
Artem <leoartemlev@yandex.ru>
2022-07-22 22:17:51 +0500

    feat: add definition system

--- LightMicroserviceModule/Program.cs
+++ LightMicroserviceModule/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using LightMicroserviceModule.Definitions.Base;
 
-app.MapGet("/", () => "Hello World!");
+try
+{
+    // created builder
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    builder.Services.AddDefinitions(builder, typeof(Program));
+
+    // create application
+    var app = builder.Build();
+    app.UseDefinitions();
+
+    // start application
+    app.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    return 1;
+}
+
+finally
+{
+    
+}
